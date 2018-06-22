91b5724a799a1fc14763c4bd5f2343ead4143dee
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-06-22 15:05:19 -0700

    add friend

--- instructor-code/src/App.js
+++ instructor-code/src/App.js
@@ -2,13 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      picture: 'alksdjfalksfj',
+      name: 'Tyler',
+      friends: [{
+        name: 'My 404 friend',
+        picture: 'https://http.cat/404'
+      }, {
+        name: 'My chill cat with the munchies',
+        picture: 'https://http.cat/420'
+      }]
+    };
+  }
+
+  changeName(name) {
+    this.setState({
+      name: name,
+    })
+  }
+
+  changePicture = (event) => {
+    this.setState({
+      picture: event.target.value,
+    })
+  }
+
+  addFriend() {
+    const newFriend = {
+      name: this.state.name,
+      picture: this.state.picture,
+    };
+    this.setState({
+      friends: this.state.friends.concat(newFriend)
+    });
+  }
+  
   render() {
     return (
-      <div>lkjsdlkfjasdklfj</div>
+      <div>
+        Name: <input value={this.state.name} onChange={(event) => this.changeName(event.target.value)} />
+        Picture: <input value={this.state.picture} onChange={(event) => this.changePicture(event)} />
+        <button onClick={() => this.addFriend()}>Add friend</button>
+        <div>
+          {this.state.friends.map(friend => {
+            return (
+              <p><img src={friend.picture} alt={friend.name} /> {friend.name}</p>
+            )
+          })}
+        </div>
+      </div>
     );
   }
 }
 
 export default App;
-
-export const myString = 'something';
--- src/App.js
+++ src/App.js
@@ -2,9 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      picture: '',
+      name: '',
+      friends: [{
+        name: 'my 404 friend',
+        picture: 'https://http.cat/404'},
+        {name: 'my cat with the munchies',
+        picture: 'https://http.cat/420'
+      }]
+    };
+  }
+
+changeName(event){
+  console.log(event.target.value);
+  this.setState({
+    name: event.target.value
+  })
+}
+changePicture(event){
+  console.log(event.target.value);
+  this.setState({
+    picture: event.target.value
+  })
+}
+
+addFriend(){
+  const newFriend = {
+    name: this.state.name,
+    picture: this.state.picture
+  };
+  this.setState({
+    friends: this.state.friends.concat(newFriend)
+  });
+}
+
   render() {
     return (
-      <div></div>
+      <div>
+        name: <input placeholder='SlowLaneFamilia' value={this.state.name} onChange = {(event) =>
+        this.changeName(event)}/>
+        picture: <input placeholder='first name' value={this.state.picture} onChange = {(event) =>
+        this.changePicture(event)}/>
+        <button onClick={() => this.addFriend()}>Add Friend</button>
+        <div>
+          {this.state.friends.map(friend => {
+          return <div>
+                    <img src={friend.picture}/>
+                  </div>
+          })}
+        </div>
+      </div>
     );
   }
 }
